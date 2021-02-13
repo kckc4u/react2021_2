5e69454f2ce4a008a4bd9639a35050af78a2a10b
kckc4u <vinodvaishnav@MacBook-Pro.local>
2021-02-13 23:39:52 +0530

    Setup rounting mechanism and url for menu items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 import Homepage from './pages/homepage.component.jsx';
 import './App.css';
 
+const HatsPage = () => <div><h1>Hats</h1></div>
+
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/hats" component={HatsPage} />
+    </Switch>
   );
 }
 
