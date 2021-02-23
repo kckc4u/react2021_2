62583e5253010ac6788eb9f4730ecf4ecdc15e10
kckc4u <vinodvaishnav@MacBook-Pro.local>
2021-02-23 11:22:55 +0530

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Homepage from './pages/homepage/homepage.component.jsx';
-import Shop from './pages/shop/shop.component.jsx';
+import Homepage from "./pages/homepage/homepage.component.jsx";
+import Shop from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route path="/shop" component={Shop} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shop} />
+      </Switch>
+    </div>
   );
 }
 
