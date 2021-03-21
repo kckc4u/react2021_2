2f2d2f8d0d156f198a507ce8fae4152610125977
kckc4u <vinodvaishnav@MacBook-Pro.local>
2021-03-21 13:22:01 +0530

    Updateing state with reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CustomButton } from "../custom-button/custom-button.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { CartItem } from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.style.scss";
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-    {
-        cartItems.map(item => <CartItem key={item.id} item={item} />)
-    }
+      {cartItems.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
-});
+const mapStateToProps = (state) => ({ cartItems: selectCartItems(state) });
 
 export default connect(mapStateToProps)(CartDropdown);
